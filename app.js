df4bcbc3ca3f0abdd38e73b3da0db26545f9f25c
madinguyen9 <mnguyen9@scu.edu>
2024-01-29 17:50:53 -0800

    final

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+
+document.getElementById("main__btn").addEventListener("click", myFunction1);
+document.getElementById("main__btn2").addEventListener("click", myFunction2);
+
+function myFunction1(){
+    window.location="#info-sect";
+}
+
+function myFunction2(){
+    window.location= "about.html";
+}
